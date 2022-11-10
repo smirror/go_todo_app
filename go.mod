3eba8541dd26a033c365fe8b48b9d77d50a9ce0b
smirror <23025996+smirror@users.noreply.github.com>
2022-11-10 16:54:28 +0900

    section-055

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module todo_app
 
 go 1.19
+
+require golang.org/x/sync v0.1.0
--- main.go
+++ main.go
@@ -6,21 +6,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(
 			func(w http.ResponseWriter, request *http.Request) {
-				fmt.Fprintf(w, "Hello, %s", request.URL.Path[1:])
+				fmt.Fprintf(w, "Hello, %s!", request.URL.Path[1:])
 			}),
-	)
-	if err != nil {
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		// http.ErrorServerClosedはhttp.ErrorServerClosedが正常に終了したことを示す
+		if err := s.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close:%v", err)
+		}
+		return nil
+	})
+
+	// チャネルからの終了通知を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	// Goメソッドで起動した別ゴルーチンの終了を待つ。
+	return eg.Wait()
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
 		fmt.Printf("faile to terminate server: %v", err)
-		os.Exit(1)
 	}
 }
