be496958e7b26e535f084503ccf0ed49707cb15d
smirror <23025996+smirror@users.noreply.github.com>
2022-11-19 22:45:43 +0900

    fix NewMux.go

--- NewMux.go
+++ NewMux.go
@@ -6,7 +6,7 @@ func NewMux() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, request *http.Request) {
 		// 静的解析のえらーを回避するため明示的に戻り値を捨てている
-		_, _ = w.Write([]byte("hello"))
+		_, _ = w.Write([]byte(`{"status":"ok"}`))
 	})
 	return mux
 }
