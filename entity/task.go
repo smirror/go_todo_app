adc4b71da2858e94a18a8818a00e0066d5615bda
smirror <23025996+smirror@users.noreply.github.com>
2022-11-19 23:02:47 +0900

    add task.go

---
+++ entity/task.go
@@ -0,0 +1,30 @@
+package entity
+
+import "time"
+
+type TaskID int64
+type TaskStatus string
+
+const (
+	TaskStatusTodo  TaskStatus = "todo"
+	TaskStatusDoing TaskStatus = "doing"
+	TaskStatusDone  TaskStatus = "done"
+)
+
+type Task struct {
+	ID      TaskID     `json:"id"`
+	Title   string     `json:"title"`
+	Status  TaskStatus `json:"status"`
+	Created time.Time  `json:"created"`
+}
+
+type Tasks []*Task
+
+func main() {
+	var id int64 = 1
+	_ = Task{ID: TaskID(id)}
+	// build error
+	//_ = Task{ID: id}
+
+	_ = Task{ID: 1}
+}
