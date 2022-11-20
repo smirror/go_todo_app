33170005e96494c6779dc409cf49934598b03e18
smirror <23025996+smirror@users.noreply.github.com>
2022-11-20 17:23:43 +0900

    fix localhost port

--- config/config.go
+++ config/config.go
@@ -4,7 +4,7 @@ import "github.com/caarlos0/env/v6"
 
 type Config struct {
 	Env  string `env:"TODO_ENV" envDefault:"dev"`
-	Port int    `env:"PORT" envDefault:"18080"`
+	Port int    `env:"PORT" envDefault:"18000"`
 }
 
 func New() (*Config, error) {
