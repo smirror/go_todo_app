f47f9e2112800e9b358d3e7b13ded07092535732
smirror <23025996+smirror@users.noreply.github.com>
2022-11-20 16:37:10 +0900

    section-70

--- NewMux.go
+++ NewMux.go
@@ -1,12 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+	"todo_app/handler"
+	"todo_app/store"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, request *http.Request) {
-		// 静的解析のえらーを回避するため明示的に戻り値を捨てている
+		w.Header().Set("Content-Type", "application/json; charset-utf-8")
 		_, _ = w.Write([]byte(`{"status":"ok"}`))
 	})
+	v := validator.New()
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServerHTTP)
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServerHTTP)
 	return mux
 }
