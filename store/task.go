71c625616870b4a66579fbdf0a482b5671b6768e
smirror <23025996+smirror@users.noreply.github.com>
2022-11-26 21:24:25 +0900

    add ListTasks

---
+++ store/task.go
@@ -0,0 +1,18 @@
+package store
+
+import (
+	"context"
+	"github.com/jmoiron/sqlx"
+	"todo_app/entity"
+)
+
+func (r *Repository) ListTasks(
+	ctx context.Context, db *sqlx.DB,
+) (entity.Tasks, error) {
+	tasks := make(entity.Tasks, 0)
+	sql := `SELECT id, title, status, created, modified FROM task;`
+	if err != db.SelectContext(ctx, &tasks, sql); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
