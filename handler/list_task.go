2f59d7127f9c4dd41e639f9e8af408e35a90f192
smirror <23025996+smirror@users.noreply.github.com>
2022-11-28 22:31:55 +0900

    change ListTask struct

--- handler/list_task.go
+++ handler/list_task.go
@@ -1,13 +1,15 @@
 package handler
 
 import (
+	"github.com/jmoiron/sqlx"
 	"net/http"
 	"todo_app/entity"
 	"todo_app/store"
 )
 
 type ListTask struct {
-	Store *store.TaskStore
+	DB   *sqlx.DB
+	Repo store.Repository
 }
 
 type task struct {
@@ -18,7 +20,14 @@ type task struct {
 
 func (lt *ListTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	tasks := lt.Store.All()
+	tasks, err := lt.Repo.ListTasks(ctx, lt.DB)
+	if err != nil {
+		ResponsedJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+
 	rsp := []task{}
 	for _, t := range tasks {
 		rsp = append(rsp, task{
