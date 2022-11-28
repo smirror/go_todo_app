a09bea6daa7bc4a6108d5ba7335987db5744951e
smirror <23025996+smirror@users.noreply.github.com>
2022-11-28 22:36:20 +0900

    change newMux

--- NewMux.go
+++ NewMux.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"context"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
 	"net/http"
+	"todo_app/clock"
+	"todo_app/config"
 	"todo_app/handler"
 	"todo_app/store"
 )
 
-func NewMux() http.Handler {
+func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
 	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, request *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset-utf-8")
 		_, _ = w.Write([]byte(`{"status":"ok"}`))
 	})
 	v := validator.New()
-	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	db, cleanup, err := store.New(ctx, cfg)
+	if err != nil {
+		return nil, nil, err
+	}
+	r := store.Repository{Clocker: clock.RealClock{}}
+	at := &handler.AddTask{DB: db, Repo: r, Validator: v}
 	mux.Post("/tasks", at.ServeHTTP)
-	lt := &handler.ListTask{Store: store.Tasks}
+	lt := &handler.ListTask{DB: db, Repo: r}
 	mux.Get("/tasks", lt.ServeHTTP)
-	return mux
+	return mux, cleanup, nil
 }
