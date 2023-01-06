67654b9d6e50ebb875df50674b2278ddab6266ad
smirror <23025996+smirror@users.noreply.github.com>
2023-01-06 23:28:56 +0900

    add NewJWTer func

--- auth/jwt.go
+++ auth/jwt.go
@@ -2,10 +2,54 @@ package auth
 
 import (
 	_ "embed"
+	"fmt"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"golang.org/x/net/context"
+	"todo_app/clock"
+	"todo_app/entity"
 )
 
 //go:embed cert/key
-var rawPrivkey []byte
+var rawPrivKey []byte
 
 //go:embed cert/key.pub
-var rawPubkey []byte
+var rawPubKey []byte
+
+type JWTer struct {
+	PrivateKey, PublixKey jwk.Key
+	Store                 Store
+	Clocker               clock.Clocker
+}
+
+//go:generate go run github.com/matryer/moq -out moq_test.go . Store
+type Store interface {
+	Save(ctx context.Context, key string, userID entity.UserID) error
+	Load(ctx context.Context, key string) (entity.UserID, error)
+}
+
+func NewJWTer(s Store) (*JWTer, error) {
+	j := &JWTer{Store: s}
+	privkey, err := parse(rawPrivKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse private key: %w", err)
+	}
+
+	pubkey, err := parse(rawPubKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse public key: %w", err)
+	}
+
+	j.PrivateKey = privkey
+	j.PublixKey = pubkey
+	j.Clocker = clock.RealClocker{}
+	return j, nil
+}
+
+func parse(rawkey []byte) (jwk.Key, error) {
+	key, err := jwk.ParseKey(rawkey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse key: %w", err)
+	}
+
+	return key, nil
+}
--- auth/jwt_test.go
+++ auth/jwt_test.go
@@ -7,12 +7,12 @@ import (
 
 func TestEmbedded(t *testing.T) {
 	want := []byte("ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDgMsu3oTWSLZffs3yRfglFnrPIfx/aAGUE11VkqTutw smirror@LAPTOP-1NQ54DHH")
-	if !bytes.Contains(rawPubkey, want) {
-		t.Errorf("want %s, but got %s", want, rawPubkey)
+	if !bytes.Contains(rawPubKey, want) {
+		t.Errorf("want %s, but got %s", want, rawPubKey)
 	}
 
 	want = []byte("-----BEGIN OPENSSH PRIVATE KEY-----")
-	if !bytes.Contains(rawPrivkey, want) {
-		t.Errorf("want %s, but got %s", want, rawPrivkey)
+	if !bytes.Contains(rawPrivKey, want) {
+		t.Errorf("want %s, but got %s", want, rawPrivKey)
 	}
 }
