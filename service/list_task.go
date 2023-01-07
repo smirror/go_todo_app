59a20223238057ddc5eabb2ece56f92ba4e29b32
smirror <23025996+smirror@users.noreply.github.com>
2023-01-07 15:27:34 +0900

    fix
    back codes

--- service/list_task.go
+++ service/list_task.go
@@ -3,8 +3,6 @@ package service
 import (
 	"context"
 	"fmt"
-	"todo_app/auth"
-
 	"todo_app/entity"
 	"todo_app/store"
 )
@@ -15,11 +13,7 @@ type ListTask struct {
 }
 
 func (l *ListTask) ListTasks(ctx context.Context) (entity.Tasks, error) {
-	id, ok := auth.GetUserID(ctx)
-	if !ok {
-		return nil, fmt.Errorf("user_id not found")
-	}
-	ts, err := l.Repo.ListTasks(ctx, l.DB, id)
+	ts, err := l.Repo.ListTasks(ctx, l.DB)
 	if err != nil {
 		return nil, fmt.Errorf("failed to list: %w", err)
 	}
