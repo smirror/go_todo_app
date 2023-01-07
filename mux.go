758eae5d43fbfa726655de1cabf279423e50ef30
smirror <23025996+smirror@users.noreply.github.com>
2023-01-07 16:28:43 +0900

    add Task add & list at login

--- mux.go
+++ mux.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"todo_app/auth"
 	"todo_app/clock"
 	"todo_app/config"
 	"todo_app/service"
@@ -24,18 +25,18 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 	if err != nil {
 		return nil, cleanup, err
 	}
-	r := store.Repository{Clocker: clock.RealClocker{}}
 
-	at := &handler.AddTask{
-		Service:   &service.AddTask{DB: db, Repo: &r},
-		Validator: v,
+	clocker := clock.RealClocker{}
+	r := store.Repository{Clocker: clocker}
+	rcli, err := store.NewKVS(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
 	}
-	mux.Post("/tasks", at.ServeHTTP)
 
-	lt := &handler.ListTask{
-		Service: &service.ListTask{DB: db, Repo: &r},
+	jwter, err := auth.NewJWTer(rcli, clocker)
+	if err != nil {
+		return nil, cleanup, err
 	}
-	mux.Get("/tasks", lt.ServeHTTP)
 
 	ru := &handler.RegisterUser{
 		Service:   &service.RegisterUser{DB: db, Repo: &r},
@@ -43,5 +44,18 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 	}
 	mux.Post("/register", ru.ServeHTTP)
 
+	at := &handler.AddTask{
+		Service:   &service.AddTask{DB: db, Repo: &r},
+		Validator: v,
+	}
+	lt := &handler.ListTask{
+		Service: &service.ListTask{DB: db, Repo: &r},
+	}
+
+	mux.Route("/tasks", func(r chi.Router) {
+		r.Use(handler.AuthMiddleware(jwter))
+		r.Post("/", at.ServeHTTP)
+		r.Get("/", lt.ServeHTTP)
+	})
 	return mux, cleanup, nil
 }
